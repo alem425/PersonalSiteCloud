9434b09c3517b2301aae7b977cc9a064b9d4fb69
alem425 <alexandermorgan364@gmail.com>
2025-04-18 02:40:27 -0400

    comments

--- backend/functions/function_app.py
+++ backend/functions/function_app.py
@@ -1,4 +1,6 @@
-import azure.functions as func
+#Python Version 3.11
+#NPM Install -g azure-functions-core-tools@4 --unsafe-perm true
+import azure.functions as func # Make sure to pip install requirements.txt
 import datetime
 import json
 import logging
