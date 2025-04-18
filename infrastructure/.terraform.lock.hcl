13fbf12073a819b67d0db65d5dd98e4d2800f646
alem425 <alexandermorgan364@gmail.com>
2025-04-18 02:34:36 -0400

    full stack setup

--- infrastructure/.terraform.lock.hcl
+++ infrastructure/.terraform.lock.hcl
@@ -3,9 +3,10 @@
 
 provider "registry.terraform.io/hashicorp/azurerm" {
   version     = "3.0.0"
-  constraints = "3.0.0"
+  constraints = ">= 3.0.0, < 4.0.0"
   hashes = [
     "h1:F6MN8AR+JO7S8o/t0/THuUakbcPn0pecCP6kYS0y1kU=",
+    "h1:zwMmdRiMqMZNcnb98s2Uu5vKgIIHCYKJc7qpeMeUjPw=",
     "zh:23a039a606cc194594f7c15cd8deef15c5183e11a40e96adee2f7317dbfa18aa",
     "zh:414890618efc6caccf60b81fcce18a7e69a6d81599678d24f538d53726f49c57",
     "zh:7c9a5d3c416766c6f624e186ee2f5b216dd5a9ffef40bfea42ceccf2b217e0d3",
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,51 +1,128 @@
+#SERVICES NEEDED:
+# 1. Azure Function App
+# 2. Azure Cosmos DB
+# 3. Azure Storage Account
+# 4. Azure Resource Group
+# 5. Azure App Service Plan
+# 6. Azure Cosmos DB SQL API Database
+# 7. Azure Cosmos DB SQL API Container
+# 8 Azure Blob Storage
+
 terraform {
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 3.0.0, < 4.0"
     }
   }
-  
+
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg-Personal_Portfolio" {
-  name = "personal_portfolio_group"
-  location = "East US"
+# Big OL' Resource Group
+resource "azurerm_resource_group" "rg-personal_portfolio" {
+  name     = "rg-personal_portfolio"
+  location = "Canada Central"
 }
 
-resource "azurerm_storage_account" "Personal_Portfolio_FuncAcct"{
-  name = " straccnt-Personal_Portfolio"
-  resource_group_name = azurerm_resource_group.rg-Personal_Portfolio.name
-  location = azurerm_resource_group.rg-Personal_Portfolio.location
-  account_tier = "Standard"
-  account_replication
+# Azure Storage Account setup for Function App
+resource "azurerm_storage_account" "personal_portfolio_func_blob_acct" {
+  name                     = "straccntportfolio"
+  resource_group_name      = azurerm_resource_group.rg-personal_portfolio.name
+  location                 = azurerm_resource_group.rg-personal_portfolio.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
 }
-resource "azurerm_app_service_plan" "Personal_Portfolio_SP"{
-  name = "personal_portfolio_function_service_plan"
-  resource_group_name = azurerm_resource_group.rg-Personal_Portfolio.name
-  location = azurerm_resource_group.rg-Personal_Portfolio.location
-  os_type = "Linux"
-  sku_name = "B1"
+# Azure Function App setup
+resource "azurerm_app_service_plan" "personal_portfolio_sp" {
+  name                = "personal_portfolio_function_service_plan"
+  resource_group_name = azurerm_resource_group.rg-personal_portfolio.name
+  location            = azurerm_resource_group.rg-personal_portfolio.location
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
 }
 
-resource "azurerm_linux_function_app" "func-Personal-Portfolio"{
-  name = "Personal-Portfolio-Function-App"
-  resource_group_name = azurerm_resource_group.rg-Personal_Portfolio.name
-  location = azurerm_resource_group.rg-Personal-Portfolio.location
+resource "azurerm_linux_function_app" "func-personal-portfolio" {
+  name                = "personal-portfolio-function-app"
+  resource_group_name = azurerm_resource_group.rg-personal_portfolio.name
+  location            = azurerm_resource_group.rg-personal_portfolio.location
 
-  storage_account_name = azurerm_storage_account.Personal_Portfolio_FuncAcct.name
-  storage_account_access_key = azurerm_storage_account.Personal_Portfolio_FuncAcct.primary_access_key
-  service_plan_id = azurerm_app_service_plan.Personal_Portfolio_SP.id
+  storage_account_name       = azurerm_storage_account.personal_portfolio_func_blob_acct.name
+  storage_account_access_key = azurerm_storage_account.personal_portfolio_func_blob_acct.primary_access_key
+  service_plan_id            = azurerm_app_service_plan.personal_portfolio_sp.id
 
   site_config {}
 }
 
-resource "azurerm_cosmosdb_account" "cosmos-Personal-Portfolio"{
-  name = "Personal_PortfolioDB"
-  location = azurerm_resource_group.rg_Personal_Portfolio
+# Cosmos DB Account setup
+resource "azurerm_cosmosdb_account" "cosmos-personal-portfolio" {
+  name                         = "personal-portfolio-cosmosdb"
+  location                     = azurerm_resource_group.rg-personal_portfolio.location
+  resource_group_name          = azurerm_resource_group.rg-personal_portfolio.name
+  offer_type                   = "Standard"
+  kind                         = "GlobalDocumentDB"
+  public_network_access_enabled = true
+
+  consistency_policy {
+    consistency_level       = "Session"
+    max_interval_in_seconds = 5
+    max_staleness_prefix    = 100
+  }
+
+  geo_location {
+    location = azurerm_resource_group.rg-personal_portfolio.location
+    failover_priority = 0
+  }
+}
+
+# Cosmos DB SQL API Database setup
+resource "azurerm_cosmosdb_sql_database" "cosmos-personal-portfolio-db" {
+  name                = "portfolio-db"
+  resource_group_name = azurerm_resource_group.rg-personal_portfolio.name
+  account_name        = azurerm_cosmosdb_account.cosmos-personal-portfolio.name
+  throughput          = 400
+
+  # depends_on = [azurerm_cosmosdb_account.cosmos-personal-portfolio]
+}
+
+# DB Container for Projects
+resource "azurerm_cosmosdb_sql_container" "projects" {
+  name                = "projects"
+  resource_group_name = azurerm_resource_group.rg-personal_portfolio.name
+  account_name        = azurerm_cosmosdb_account.cosmos-personal-portfolio.name
+  database_name       = azurerm_cosmosdb_sql_database.cosmos-personal-portfolio-db.name
+
+  partition_key_path    = "/projectsID"
+  partition_key_version = 2
+
+  # depends_on = [azurerm_cosmosdb_sql_database.cosmos-personal-portfolio-db]
+}
+
+# DB Container for my user info
+resource "azurerm_cosmosdb_sql_container" "admin" {
+  name                = "admin"
+  resource_group_name = azurerm_resource_group.rg-personal_portfolio.name
+  account_name        = azurerm_cosmosdb_account.cosmos-personal-portfolio.name
+  database_name       = azurerm_cosmosdb_sql_database.cosmos-personal-portfolio-db.name
+
+  partition_key_path    = "/adminID"
+  partition_key_version = 2
+
+  # depends_on = [azurerm_cosmosdb_sql_database.cosmos-personal-portfolio-db]
+}
+
+# Blob Storage setup
+resource "azurerm_storage_container" "blob-Personal-Portfolio" {
+  name                  = "portfolio-blob-storage"
+  storage_account_name  = azurerm_storage_account.personal_portfolio_func_blob_acct.name
+  container_access_type = "blob"
+  # depends_on            = [azurerm_storage_account.Personal_Portfolio_FuncAcct]
+
 }
