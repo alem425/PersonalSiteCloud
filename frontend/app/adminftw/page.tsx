01cd1b2aa46c76360d3a200dad78c918bf8766e8
alem425 <alexandermorgan364@gmail.com>
2025-05-22 20:13:46 -0400

    external link requirement removal

--- frontend/app/adminftw/page.tsx
+++ frontend/app/adminftw/page.tsx
@@ -202,7 +202,6 @@ export default function AdminPage() {
                 value={website}
                 onChange={(e) => setWebsite(e.target.value)}
                 className="w-full px-4 py-2 bg-black/50 border border-gray-700 rounded-lg focus:outline-none focus:border-blue-500"
-                required
               />
             </div>
             </div>
